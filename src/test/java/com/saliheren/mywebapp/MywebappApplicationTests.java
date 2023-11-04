593113a0eaf071071c8c36a41646a132a7e6d44b
JNewsted1986 <birincioglu.saliheren@gmail.com>
2023-11-04 17:05:27 +0300

    First commit

---
+++ src/test/java/com/saliheren/mywebapp/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saliheren.mywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

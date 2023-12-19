2949b2bc33694421eea7f17eb8c40fca7e763ffa
Salih Eren <birincioglu.saliheren@gmail.com>
2023-12-20 01:55:57 +0300

    spring-web-project-done

--- Dev/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ Dev/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,13 +1,22 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name hould have atleast 2 characters")
     @NotBlank
@@ -16,10 +25,18 @@ public class User {
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
+
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
@@ -44,6 +61,14 @@ public class User {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
---
+++ Dev/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into user_details(id,birth_date,name)
+values (1,current_date(),'Eren');
+insert into user_details(id,birth_date,name)
+values (2,current_date(),'Salih');
+insert into user_details(id,birth_date,name)
+values (3,current_date(),'Birinci');
+
+insert into post(id,description,user_id)
+values (201,'Learn AWS',1);
+insert into post(id,description,user_id)
+values (202,'Learn Devops',1);
+insert into post(id,description,user_id)
+values (203,'Learn AI',2);

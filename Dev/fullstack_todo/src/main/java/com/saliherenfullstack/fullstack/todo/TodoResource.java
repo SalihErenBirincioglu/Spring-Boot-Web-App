8d47eeaa5daee51a5cfed5147339998bc3f8380f
Salih Eren <birincioglu.saliheren@gmail.com>
2024-02-07 03:06:25 +0300

    deleteApi added

--- Dev/fullstack_todo/src/main/java/com/saliherenfullstack/fullstack/todo/TodoResource.java
+++ Dev/fullstack_todo/src/main/java/com/saliherenfullstack/fullstack/todo/TodoResource.java
@@ -1,12 +1,12 @@
 package com.saliherenfullstack.fullstack.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TodoResource {
 
     private TodoService todoService;
@@ -18,4 +18,17 @@ public class TodoResource {
     public List<Todo> retrieveTodos(@PathVariable String username){
         return todoService.findByUsername(username);
     }
+
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String username,@PathVariable int id){
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id){
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
